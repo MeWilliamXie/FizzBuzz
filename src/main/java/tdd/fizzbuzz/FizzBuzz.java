da5d481d10b8661a4553bdc15da86c7adaf90b82
WILLIAM XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEWI3@OOCL.COM>
2022-07-21 13:45:12 +0800

    feat: finished should_say_Fizz_when_given_3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,7 +1,14 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+
     public String countOff(int i) {
-        return null;
+        String answer = "";
+        if(i%3==0){
+            answer += "Fizz";
+        }
+        return answer.length() == 0?"" + i:answer;
     }
+
+
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -10,9 +10,20 @@ public class FizzBuzzTest {
     public void should_say_order_number_when_given_1(){
         FizzBuzz fizzBuzz = new FizzBuzz();
 
-        String answer = fizzBuzz.countOff(3);
+        String answer = fizzBuzz.countOff(1);
 
         assertEquals("1",answer);
     }
+
+    @Test
+    public void should_say_Fizz_when_given_3(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String answer = fizzBuzz.countOff(3);
+
+        assertEquals("Fizz",answer);
+    }
+
+
 
 }
