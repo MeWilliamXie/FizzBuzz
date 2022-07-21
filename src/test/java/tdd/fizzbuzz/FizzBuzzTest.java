e3f65e83c9a9ba69e42ee425f769dee55c751cf2
WILLIAM XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEWI3@OOCL.COM>
2022-07-21 11:59:58 +0800

    test: should_say_order_number_when_given_1

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,18 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void should_say_order_number_when_given_1(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String answer = fizzBuzz.countOff(3);
+
+        assertEquals("1",answer);
+    }
+
 }
