4170e31dc6d46bd5d0accc1749e24fd87ed70795
WILLIAM XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEWI3@OOCL.COM>
2022-07-21 13:58:19 +0800

    format code

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,16 +4,16 @@ public class FizzBuzz {
 
     public String countOff(int i) {
         String answer = "";
-        if(i%3==0){
+        if (i % 3 == 0) {
             answer += "Fizz";
         }
-        if(i%5 == 0){
+        if (i % 5 == 0) {
             answer += "Buzz";
         }
-        if(i%7 == 0){
+        if (i % 7 == 0) {
             answer += "Whizz";
         }
-        return answer.length() == 0?"" + i:answer;
+        return answer.length() == 0 ? "" + i : answer;
     }
 
 
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -7,75 +7,75 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzTest {
 
     @Test
-    public void should_say_order_number_when_countOff_given_1(){
+    public void should_say_order_number_when_countOff_given_1() {
         FizzBuzz fizzBuzz = new FizzBuzz();
 
         String answer = fizzBuzz.countOff(1);
 
-        assertEquals("1",answer);
+        assertEquals("1", answer);
     }
 
     @Test
-    public void should_say_Fizz_when_countOff_given_3(){
+    public void should_say_Fizz_when_countOff_given_3() {
         FizzBuzz fizzBuzz = new FizzBuzz();
 
         String answer = fizzBuzz.countOff(3);
 
-        assertEquals("Fizz",answer);
+        assertEquals("Fizz", answer);
     }
 
     @Test
-    public void should_say_Buzz_when_countOff_given_5(){
+    public void should_say_Buzz_when_countOff_given_5() {
         FizzBuzz fizzBuzz = new FizzBuzz();
 
         String answer = fizzBuzz.countOff(5);
 
-        assertEquals("Buzz",answer);
+        assertEquals("Buzz", answer);
     }
 
     @Test
-    public void should_say_Whizz_when_countOff_given_7(){
+    public void should_say_Whizz_when_countOff_given_7() {
         FizzBuzz fizzBuzz = new FizzBuzz();
 
         String answer = fizzBuzz.countOff(7);
 
-        assertEquals("Whizz",answer);
+        assertEquals("Whizz", answer);
     }
 
     @Test
-    public void should_say_FizzBuzz_when_countOff_given_15(){
+    public void should_say_FizzBuzz_when_countOff_given_15() {
         FizzBuzz fizzBuzz = new FizzBuzz();
 
         String answer = fizzBuzz.countOff(15);
 
-        assertEquals("FizzBuzz",answer);
+        assertEquals("FizzBuzz", answer);
     }
 
     @Test
-    public void should_say_FizzWhizz_when_countOff_given_21(){
+    public void should_say_FizzWhizz_when_countOff_given_21() {
         FizzBuzz fizzBuzz = new FizzBuzz();
 
         String answer = fizzBuzz.countOff(21);
 
-        assertEquals("FizzWhizz",answer);
+        assertEquals("FizzWhizz", answer);
     }
 
     @Test
-    public void should_say_BuzzWhizz_when_countOff_given_35(){
+    public void should_say_BuzzWhizz_when_countOff_given_35() {
         FizzBuzz fizzBuzz = new FizzBuzz();
 
         String answer = fizzBuzz.countOff(35);
 
-        assertEquals("BuzzWhizz",answer);
+        assertEquals("BuzzWhizz", answer);
     }
 
     @Test
-    public void should_say_FizzBuzzWhizz_when_countOff_given_105(){
+    public void should_say_FizzBuzzWhizz_when_countOff_given_105() {
         FizzBuzz fizzBuzz = new FizzBuzz();
 
         String answer = fizzBuzz.countOff(105);
 
-        assertEquals("FizzBuzzWhizz",answer);
+        assertEquals("FizzBuzzWhizz", answer);
     }
 
 
